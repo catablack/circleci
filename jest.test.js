000303b82b42f2538b24324e843e233a217f735e
Catalin Pop <catalin.pop@tapptitude.com>
2021-12-29 15:12:51 +0200

    Multiply test

--- jest.test.js
+++ jest.test.js
@@ -5,3 +5,7 @@ test('adds 1 + 2 to equal 3', () => {
 test('multiply 2 * 2 to equal 4', () => {
   expect(2 * 2).toBe(4);
 });
+
+test('multiply 2 * 3 to not equal 4', () => {
+  expect(2 * 3).toBe(6);
+});
